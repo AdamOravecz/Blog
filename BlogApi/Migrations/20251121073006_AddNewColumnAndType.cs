20ce1062e447c7ec5ec11fb5031308acc85aea0f
Oravecz Adam <oravecza@kkszki.hu>
2025-11-21 08:51:07 +0100

    Second Phase done

---
+++ BlogApi/Migrations/20251121073006_AddNewColumnAndType.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BlogApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddNewColumnAndType : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Bloggers",
+                type: "varchar(30)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "longtext");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Email",
+                table: "Bloggers",
+                type: "text",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "longtext");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Password",
+                table: "Bloggers",
+                type: "longtext",
+                nullable: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Password",
+                table: "Bloggers");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Bloggers",
+                type: "longtext",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "varchar(30)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Email",
+                table: "Bloggers",
+                type: "longtext",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "text");
+        }
+    }
+}
--- BlogApi/Migrations/20251121074525_PostTableAndConnectionIsCreated.Designer.cs
+++ BlogApi/Migrations/20251121074525_PostTableAndConnectionIsCreated.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using BlogApi.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace BlogApi.Migrations
 {
     [DbContext(typeof(BlogDbConntext))]
-    partial class BlogDbConntextModelSnapshot : ModelSnapshot
+    [Migration("20251121074525_PostTableAndConnectionIsCreated")]
+    partial class PostTableAndConnectionIsCreated
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
