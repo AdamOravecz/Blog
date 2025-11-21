50452a67c3874dace96ad09d8d8cc0568bce5533
Oravecz Adam <oravecza@kkszki.hu>
2025-11-21 08:17:48 +0100

    Blogger model is created

---
+++ BlogApi/Models/BlogDbConntext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace BlogApi.Models
+{
+    public class BlogDbConntext : DbContext
+    {
+        public BlogDbConntext()
+        {
+        }
+        public BlogDbConntext(DbContextOptions options) : base(options)
+        {
+        }
+        public DbSet<Blogger> Bloggers { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseMySQL("Server=localhost;Database=Blog;user=root;password=");
+        }
+    }
+}
